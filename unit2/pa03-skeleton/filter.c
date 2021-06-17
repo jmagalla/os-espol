b2bea37681e38f1300f268e9505d6b29ef94ff65
Jorge Magallanes <jmagalla@espol.edu.ec>
2021-06-17 18:01:57 -0500

    cambios en filter.c

--- unit2/pa03-skeleton/filter.c
+++ unit2/pa03-skeleton/filter.c
@@ -1,7 +1,10 @@
 #include "bmp.h"
 
-void apply(BMP_Image * imageIn, BMP_Image * imageOut);
+void apply(BMP_Image * imageIn, BMP_Image * imageOut)
+{}
 
-void applyParallel(BMP_Image * imageIn, BMP_Image * imageOut, int boxFilter[3][3], int numThreads);
+void applyParallel(BMP_Image * imageIn, BMP_Image * imageOut, int boxFilter[3][3], int numThreads)
+{}
 
-void *filterThreadWorker(void * args);
+void *filterThreadWorker(void * args)
+{}
